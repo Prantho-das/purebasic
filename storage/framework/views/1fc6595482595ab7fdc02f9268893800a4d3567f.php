668913bd093188de7700d5b62b682f46f11492e7
Prantho-das <pranthokumardas786@gmail.com>
2025-10-10 11:52:44 +0600

    Remove storage files from git tracking

--- storage/framework/views/1fc6595482595ab7fdc02f9268893800a4d3567f.php
+++
@@ -1,6 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-
-    <div style="margin-top: 5rem;"><b>Admin : <?php echo e($adminOtp); ?></b></div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.register', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- storage/framework/views/7d0115d66af5c218041c1a27b9758d44c000cb5b.php
+++
@@ -1,7 +0,0 @@
-    
-    <footer id="footer" class="footer">
-
-        <h1> this is footer blade . php</h1>
-
-    </footer>
-    
