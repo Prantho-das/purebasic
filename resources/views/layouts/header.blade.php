813bf08d97f1a979ea77653ff37cced5a7239216
Mahbub-diu <mahbub.mediasoft@gmail.com>
2026-01-01 22:57:29 +0600

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,94 +3,93 @@
     <div class="container ed-container-expand">
         <div class="top-header-flex">
 
-            
+
             <div class="logo-box">
                 <a href="{{ url('/') }}">
 
                     @php
-                    $logo=get_business_setting('site_logo');
+                        $logo = get_business_setting('site_logo');
                     @endphp
                     @if ($logo)
-                    <img src="{{ asset('storage/' . $logo) }}" alt="logo">
+                        <img src="{{ asset('storage/' . $logo) }}" alt="logo">
                     @else
-                    <img src="{{ asset('assets/images/logo-light.png') }}" alt="logo">
+                        <img src="{{ asset('assets/images/logo-light.png') }}" alt="logo">
                     @endif
                 </a>
             </div>
             @php
-            $batchCategory=App\BatchCategory::all();
+                $batchCategory = App\BatchCategory::all();
             @endphp
 
-<form action="{{route('web.search')}}">
-            <div class="topheader-search-box">
-                <div class="category-select-box">
-                    <select name="batch_category" class="category-select wide">
-                        <option value="0" selected disabled>All Categories</option>
-                        @foreach($batchCategory as $bcategory)
-                        <option value="{{$bcategory->id}}">{{$bcategory->name}}</option>
-                        @endforeach
-                    </select>
+            <form action="{{ route('web.search') }}">
+                <div class="topheader-search-box">
+                    <div class="category-select-box">
+                        <select name="batch_category" class="category-select wide">
+                            <option value="0" selected disabled>All Categories</option>
+                            @foreach ($batchCategory as $bcategory)
+                                <option value="{{ $bcategory->id }}">{{ $bcategory->name }}</option>
+                            @endforeach
+                        </select>
 
-                </div>
-                <div class="topsearch-box">
-                    {{-- <form action="{{route('web.search')}}"> --}}
+                    </div>
+                    <div class="topsearch-box">
+                        {{-- <form action="{{route('web.search')}}"> --}}
                         <input name="search" type="text" placeholder="Search your courses">
                         <button class="header-search-btn">search <i class="fa-light fa-magnifying-glass"></i></button>
-                    {{-- </form> --}}
+                        {{-- </form> --}}
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
             <div class="topbar-info">
                 <div class="topbar-social-link">
                     @php
-                    $row = \DB::table('business_settings')
-                    ->where('type', 'social_media_links')
-                    ->first(['value']);
-                    $socialTypes=['facebook', 'twitter', 'instagram', 'youtube'];
-                    if ($row) {
-                    $settingsArray = json_decode($row->value, true) ?? [];
-                    }else{
-                    $settingsArray = [];
-                    }
+                        $row = \DB::table('business_settings')
+                            ->where('type', 'social_media_links')
+                            ->first(['value']);
+                        $socialTypes = ['facebook', 'twitter', 'instagram', 'youtube'];
+                        if ($row) {
+                            $settingsArray = json_decode($row->value, true) ?? [];
+                        } else {
+                            $settingsArray = [];
+                        }
 
+                        // Build assoc array from the array of objects (link_type as key)
+                        $settings = [];
+                        foreach ($settingsArray as $item) {
+                            if (isset($item['link_type'])) {
+                                $settings[$item['link_type']] = $item['link'] ?? '';
+                            }
+                        }
 
-                    // Build assoc array from the array of objects (link_type as key)
-                    $settings = [];
-                    foreach ($settingsArray as $item) {
-                    if (isset($item['link_type'])) {
-                    $settings[$item['link_type']] = $item['link'] ?? '';
-                    }
-                    }
-
-                    // Merge defaults (ensure all types exist, even if empty)
-                    foreach ($socialTypes as $type) {
-                    if (!isset($settings[$type])) {
-                    $settings[$type] = '';
-                    }
-                    }
-                    $iconMap = [
-                    'facebook' => 'fa-facebook-f',
-                    'twitter' => 'fa-twitter',
-                    'instagram' => 'fa-instagram',
-                    'youtube' => 'fa-youtube',
-                    ];
+                        // Merge defaults (ensure all types exist, even if empty)
+                        foreach ($socialTypes as $type) {
+                            if (!isset($settings[$type])) {
+                                $settings[$type] = '';
+                            }
+                        }
+                        $iconMap = [
+                            'facebook' => 'fa-facebook-f',
+                            'twitter' => 'fa-twitter',
+                            'instagram' => 'fa-instagram',
+                            'youtube' => 'fa-youtube',
+                        ];
                     @endphp
                     <ul class="social-links">
-                        @foreach($socialTypes as $type)
-                        @if(!empty($settings[$type] ?? ''))
-                        <li>
-                            <a href="{{ $settings[$type] }}" target="_blank" rel="noopener noreferrer">
-                                <i class="fa-brands {{ $iconMap[$type] ?? 'fa-' . $type }}"></i>
-                            </a>
-                        </li>
-                        @else
-                        {{-- Optional: Show inactive with # href or skip entirely --}}
-                        <li>
-                            <a href="#">
-                                <i class="fa-brands {{ $iconMap[$type] ?? 'fa-' . $type }}"></i>
-                            </a>
-                        </li>
-                        @endif
+                        @foreach ($socialTypes as $type)
+                            @if (!empty($settings[$type] ?? ''))
+                                <li>
+                                    <a href="{{ $settings[$type] }}" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands {{ $iconMap[$type] ?? 'fa-' . $type }}"></i>
+                                    </a>
+                                </li>
+                            @else
+                                {{-- Optional: Show inactive with # href or skip entirely --}}
+                                <li>
+                                    <a href="#">
+                                        <i class="fa-brands {{ $iconMap[$type] ?? 'fa-' . $type }}"></i>
+                                    </a>
+                                </li>
+                            @endif
                         @endforeach
                     </ul>
 
@@ -98,13 +97,13 @@
                 </div>
                 <div class="topbar-buttons">
                     @php
-                    $id=Session:: get('id');
-                @endphp
-                @if($id)
-                    <button type="button" class="register-btn">Register</button>
-                    <a type="button" href={{url('student/login')}} class="btn login-btn">Log In</a>
+                        $id = Session::get('id');
+                    @endphp
+                    @if ($id)
+                        <button type="button" class="register-btn">Register</button>
+                        <a type="button" href={{ url('student/login') }} class="btn login-btn">Log In</a>
                     @else
-                    <a href="/profile/{{$id}}">Dashboard</a>
+                        <a href="/profile/{{ $id }}">Dashboard</a>
                     @endif
                 </div>
 
@@ -119,54 +118,52 @@
     <div class="main-header" id="main-header">
         <div class="container ed-container-expand">
             <div class="header-flex">
-
-
-
-
-
                 <div class="left-box">
                     <ul>
-                        @foreach(\App\Menu::ofType('header')->root()->active()->orderBy('sort_order')->with('children')->get() as $headerMenu)
-                        <li>
-                            <a href="{{ $headerMenu->url ?? '#' }}">{{ $headerMenu->name ?? 'Menu Item' }}
-                                @if(count($headerMenu->children))
-                                <i
-                                    class="fa-regular fa-chevron-down"></i>
-                                @endif
+                        @foreach (\App\Menu::ofType('header')->root()->active()->orderBy('sort_order')->with('children')->get() as $headerMenu)
+                            <li>
+                                <a href="{{ $headerMenu->url ?? '#' }}">{{ $headerMenu->name ?? 'Menu Item' }}
+                                    @if (count($headerMenu->children))
+                                        <i class="fa-regular fa-chevron-down"></i>
+                                    @endif
 
                                 </a>
 
-                               @if(count($headerMenu->children))
-                            <ul class="sub-menu">
-                                @foreach($headerMenu->children ?? [] as $child)
-                                <li><a href="{{ $child->url ?? '#' }}">{{ $child->name ?? 'Sub Item' }}</a></li>
-                                @endforeach
-                            </ul>
-                            @endif
-                        </li>
+                                @if (count($headerMenu->children))
+                                    <ul class="sub-menu">
+                                        @foreach ($headerMenu->children ?? [] as $child)
+                                            <li><a
+                                                    href="{{ $child->url ?? '#' }}">{{ $child->name ?? 'Sub Item' }}</a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @endif
+                            </li>
                         @endforeach
 
                     </ul>
                 </div>
                 <div class="right-box">
                     <ul>
-                        @foreach(\App\Menu::ofType('other')->root()->active()->orderBy('sort_order')->with('children')->get() as $headerMenu)
-                        <li>
-                            <a href="{{ $headerMenu->url ?? '#' }}">{{ $headerMenu->name ?? 'Menu Item' }}
-                                @if(count($headerMenu->children))
-                                <i class="fa-regular fa-chevron-down"></i>
+                        @foreach (\App\Menu::ofType('other')->root()->active()->orderBy('sort_order')->with('children')->get() as $headerMenu)
+                            <li>
+                                <a href="{{ $headerMenu->url ?? '#' }}">{{ $headerMenu->name ?? 'Menu Item' }}
+                                    @if (count($headerMenu->children))
+                                        <i class="fa-regular fa-chevron-down"></i>
+                                    @endif
+
+                                </a>
+
+                                @if (count($headerMenu->children))
+                                    <ul class="sub-menu">
+                                        @foreach ($headerMenu->children ?? [] as $child)
+                                            <li><a
+                                                    href="{{ $child->url ?? '#' }}">{{ $child->name ?? 'Sub Item' }}</a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
                                 @endif
-
-                            </a>
-
-                            @if(count($headerMenu->children))
-                            <ul class="sub-menu">
-                                @foreach($headerMenu->children ?? [] as $child)
-                                <li><a href="{{ $child->url ?? '#' }}">{{ $child->name ?? 'Sub Item' }}</a></li>
-                                @endforeach
-                            </ul>
-                            @endif
-                        </li>
+                            </li>
                         @endforeach
 
                     </ul>
