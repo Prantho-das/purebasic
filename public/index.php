985b70f8c4e4e5c023a71ea4f2ff1bdea85b8e04
root <root@srv699171.hstgr.cloud>
2026-01-02 05:33:18 +0000

    pure baseic

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('serialize_precision', -1);
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
@@ -14,9 +16,10 @@ define('LARAVEL_START', microtime(true));
 | Register The Auto Loader
 |--------------------------------------------------------------------------
 |
-| Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll simply require
-| it into the script here so we don't have to worry about manual loading.
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
 |
 */
 
@@ -28,9 +31,9 @@ require __DIR__.'/../vendor/autoload.php';
 |--------------------------------------------------------------------------
 |
 | We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it will
-| load up this application so that we can run it and send the responses
-| back to the browser and delight our users.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
 |
 */
 
@@ -43,7 +46,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 | Once we have the application, we can handle the incoming request
 | through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the application.
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
 |
 */
 
@@ -51,6 +55,8 @@ $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
-)->send();
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
